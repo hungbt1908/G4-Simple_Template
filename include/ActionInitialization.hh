ca3dc1f7157a2e31737aa4672db9e9d1c90cc6ea
hungbt-x1 <hungbt1908@gmail.com>
2024-08-31 10:58:52 +0700

    init project

---
+++ include/ActionInitialization.hh
@@ -0,0 +1,14 @@
+#ifndef ActionInitialization_h
+#define ActionInitialization_h 1
+
+#include "G4VUserActionInitialization.hh"
+#include "globals.hh"
+
+class ActionInitialization : public G4VUserActionInitialization
+{
+public:
+    ActionInitialization();
+    virtual ~ActionInitialization();
+    virtual void Build() const;
+};
+#endif
